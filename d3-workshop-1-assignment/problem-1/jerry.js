42c761eae58ce807fcac5acd1f3017430be2dee6
Matt Chase <mnchase@umass.edu>
2024-06-10 21:49:49 -0400

    finished part 1

--- d3-workshop-1-assignment/problem-1/jerry.js
+++ d3-workshop-1-assignment/problem-1/jerry.js
@@ -14,7 +14,17 @@ function makeJerry(jerry){
         square_height = height/17              
 
     jerry.forEach((row,i) => {
-        svg.selectAll('rect') // Select all <rect> elements in the DOM. Note: This is valid even if there are no <rect> elements yet.
+	svg.selectAll('row')
+		.data(row)
+		.enter()
+		.append('rect')
+		.attr('height', square_height)
+		.attr('width', square_width)
+		.attr('x', function(elem,j){return j*square_width})
+		.attr('y', function(elem,j){return i*square_height})
+		.style('fill', function(elem,j){return elem})
+    })
+		// Select all <rect> elements in the DOM. Note: This is valid even if there are no <rect> elements yet.
             /* TODO: 
                 1. Bind the row array to this selection.
                 2. Use enter() and append() to create a square for each of the 17 pixels in the row array
@@ -24,7 +34,6 @@ function makeJerry(jerry){
                 4. Style the squares
                     Hint: row is an array of rgb strings, which are valid values for the fill property in css.
                     */
-    })
     
 }
 
--- d3-workshop-1-follow-along/04_using_d3/04_using_d3.js
+++ d3-workshop-1-follow-along/04_using_d3/04_using_d3.js
@@ -6,8 +6,10 @@ let flowers = [
 ];
 
 let rect_data = [
-    { width: 80, height: 80, x: 80, y: 40, color: "purple"},
-    { width: 40, height: 40, x: 100, y: 60, color: "yellow"}
+    [{ width: 80, height: 80, x: 80, y: 40, color: "purple"},
+    { width: 40, height: 40, x: 100, y: 60, color: "yellow"}],
+    [{ width: 80, height: 80, x: 80, y: 40, color: "red"},
+    { width: 40, height: 40, x: 100, y: 60, color: "blue"}]
 ];
 
 d3.selectAll('.first-circle')
